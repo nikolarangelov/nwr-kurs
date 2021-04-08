01d5fdf7cfab9cb3c181622558aa4cf3d73dacdc
nikolarangelov <you@example.com>
2021-04-08 12:24:14 +0200

    urađen domaći

---
+++ Predavanje 8- JavaScript/js/javascript.js
@@ -0,0 +1,7 @@
+document.write("Ovo je moj prvi javascript code!");
+function radi(){
+    let x = document.querySelector('#content');
+    x.style.color = 'red';
+    x.style.fontSize = '1rem';
+}
+radi();
