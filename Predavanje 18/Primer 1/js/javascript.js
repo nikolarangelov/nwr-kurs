e3c71df9776f468095987cc3fd8fcc8a40033a00
nikolarangelov <you@example.com>
2021-05-11 19:14:06 +0200

    Dodat zadatak 1

---
+++ Predavanje 18/Primer 1/js/javascript.js
@@ -0,0 +1,19 @@
+/** For Loop (petlja)
+ * 
+ */
+
+for ( let i = 0; i < 10; i++) {
+  //  console.log('Brojac koji se izvsava 10 puta')
+    console.log(i)
+}
+
+/**
+ * Zadatak 1
+ * Napisati for petlju koja se izvrsava 20 puta i
+ * stampa poruku u sledecem formatu
+ * 
+ * Interakcija br: <broj_interakcije>
+ */
+for (let i=1; i < 21; i++) {
+    console.log(`Interakcija broj ${i}`)
+}
